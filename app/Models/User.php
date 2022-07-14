2d8ddaebec2a2a92f03a15cf2b716cf45558c306
Miftahul Hasanah <miftahulhasanah174@gmail.com>
2022-07-14 08:38:21 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Jenssegers\Mongodb\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
@@ -18,6 +18,8 @@ class User extends Authenticatable implements JWTSubject
      *
      * @var array<int, string>
      */
+
+    protected $connection = 'mongodb';
     protected $fillable = [
         'name',
         'email',
